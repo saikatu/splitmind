357d1c9fddcfb05021ad1a966f8860c7bc1cb245
saikatu <saikatu@saikatu-workbook.localdomain>
2011-05-30 05:45:05 +0900

    Init repository.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Splitmind::Application.config.secret_token = '5f76e224bee23e0e94346e9486a64bfd5e7da1dd1916b7d412017adc0dbca8861e76183d8b84fe078f9d81fe0fd9ffde9584586069a89a49ba1ff4ce4e335392'
